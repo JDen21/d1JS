b6620a210e59a1be1507b8fa2ccb6d47af35ae36
Den Garcia <dengarcia@KPMcPro-152.local>
2024-02-11 13:35:22 +0800

    res.sendFile

--- sampleApp/httpApp/test/index.js
+++ sampleApp/httpApp/test/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 'post /no-send';
 function testRoute (req, res) {
   return 'route that not call send';
@@ -17,9 +18,15 @@ function asyncMethod (req, res) {
   return Promise.resolve({ asc: 'this is async response.' });
 }
 
+'get /test-static-file';
+function sendFileMethod (req, res) {
+  res.sendFile(path.join(__dirname, '../../static/test.html'));
+}
+
 module.exports = {
   testRoute,
   testRouteNoValidSend,
   testNoMethod,
-  asyncMethod
+  asyncMethod,
+  sendFileMethod
 };
